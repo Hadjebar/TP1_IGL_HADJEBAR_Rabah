3ff2d147eae290f5cde5fb9820b14ce5ddf74efd
HADJEBAR Rabah <hadjebar1997@gmail.com>
2017-10-24 18:38:25 +0200

    mettre à jour VecteurHelper

--- src/tp_igl_applivation2/VecteurHelper.java
+++ src/tp_igl_applivation2/VecteurHelper.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tp_igl_applivation2;
 
 /**
- *
+ * La classe VecteurHelper contient un ensemble de méthode sur les vecteurs
  * @author Hadjebar Rabah
  */
 public class VecteurHelper {
